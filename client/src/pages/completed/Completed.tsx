91f099c000465c1b4bb8d2853fb77224b9a24136
Dohee Lee <dojuhee2015@gmail.com>
2024-03-10 22:24:43 +0000

    add some css and onclick function

--- client/src/pages/completed/Completed.tsx
+++ client/src/pages/completed/Completed.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Trophy from '../../images/trophy.png'
 import Button from '../../components/Buttons'
+import { useNavigate } from 'react-router-dom'
 import '../../scss/main.scss'
 
 const Completed = () => {
   const score = useSelector((state: any) => state.questionSlice.score)
   const nickname = useSelector((state: any) => state.playerSlice.nickname)
+
+  const navigate = useNavigate()
+
+  const goToHome = () => {
+    navigate('/')
+  }
+
+  const goToLeaderboard = () => {
+    navigate('/leaderboard')
+  }
 
   return (
     <div className='completed-container'>
@@ -17,7 +28,7 @@ const Completed = () => {
         Check out the score in the leaderboard
       </p>
       <div className='completed-button-container'>
-        <Button title='Home' />
+        <Button title='Home' onClick={goToHome} />
         <Button title='Leaderboard' />
         <Button title='Play Again' />
       </div>
