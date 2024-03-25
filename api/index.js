8e6f9e615e692a5c354d47c1690680d9a1235fef
Dohee Lee <dojuhee2015@gmail.com>
2024-03-25 21:28:16 +0000

    remove api folder

--- api/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const app = require('./app')
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
--- api/routers/playerRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { createPlayer } = require('../controller/playerController')
-
-router.post('/', createPlayer)
-
-module.exports = router
